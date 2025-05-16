62556c1b57491aec94729abd095a92322d67da24
abdel hamed <vfor12@yahoo.com>
2025-05-16 12:32:03 +0300

    feat: Simplify landing page animation and improve button positioning

--- src/pages/landing/Landing.tsx
+++ src/pages/landing/Landing.tsx
@@ -1,24 +1,7 @@
-import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 
 function Landing() {
-  const [moveToRight, setMoveToRight] = useState(false);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      setMoveToRight(true);
-
-      const interval = setInterval(() => {
-        setMoveToRight(prev => !prev);
-      }, 9000);
-
-      return () => clearInterval(interval);
-    }, 9000);
-
-    return () => clearTimeout(timer);
-  }, []);
-
   return (
     <div className="relative w-screen h-screen overflow-hidden">
       {/* خلفية الفيديو */}
@@ -34,22 +17,16 @@ function Landing() {
       </video>
 
       {/* المحتوى فوق الفيديو */}
-      <div className="relative z-10 flex items-center justify-center w-full h-full">
+      <div className="relative z-10 w-full h-full">
         <Link to="/login">
           <motion.button
             initial={{ opacity: 0, scale: 0.8 }}
-            animate={{
-              opacity: 1,
-              scale: 1,
-              x: moveToRight ? 800 : 0,
-              y: moveToRight ? 20 : 0, // يتحرك للأعلى عندما يكون يمينًا
-            }}
+            animate={{ opacity: 1, scale: 1 }}
             transition={{
               duration: 1.2,
               ease: "easeInOut",
             }}
-            className="absolute text-lg bg-gradient-to-r from-cyan-500 to-cyan-600 hover:from-cyan-500 hover:to-cyan-600 text-white font-semibold py-3 cursor-pointer px-6 rounded-lg shadow-lg"
-            style={{ bottom: "15%", left: "20%" }}
+            className="absolute top-[77%] left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-lg bg-gradient-to-r from-cyan-400 to-cyan-300 hover:from-cyan-500 hover:to-cyan-400 text-white font-semibold py-3 px-6 rounded-lg shadow-lg"
           >
             Get Started
           </motion.button>
